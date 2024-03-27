c6a16f82b126af515a97a5c631b5dfef53fad7b5
Alexandru Lupean <alexandru.lupean@3ss.tv>
2024-03-27 10:32:12 +0200

    Added Ag-Grid, Created setup for products table using ag grid

---
+++ src/modules/auth/pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <TestTable />
+</template>
+
+<script setup lang="ts">
+import TestTable from '@/components/common/tables/TestTable.vue';
+
+
+</script>
+
+<route lang="yaml">
+name: Products
+</route>
